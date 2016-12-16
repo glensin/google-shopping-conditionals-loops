6e209a910f9517f4517a226dd6f02ea28fa09cf2
glensin <glen@glensin.com>
2016-12-17 03:50:30 +0800

    done

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,44 @@
-var data = require('../products.json');
+var data = require('../products.json')
 
 // Write your solutions below
 
-console.log(data.items);
+//
+// console.log(data.items); --Q1
+
+
+// var array = data.items
+// for (var i = 0; i < array.length; i++){
+// if ((array[i].product.inventories[0].availability)==='backorder') {
+//   console.log(array[i].product.title);
+// }
+//   } ------------Q2
+
+// var array = data.items
+// for (var i = 0; i < array.length; i++) {
+// var numberOfkind = array[i].product.images
+// if ((numberOfkind.length) > 1) {
+// console.log(array[i].product.title) }
+// } ------ Q3
+
+// var array = data.items
+// for (var i = 0; i < array.length; i++) {
+// var brand = (array[i].product.brand);
+// if (brand === "Canon") {
+//   console.log(array[i]);}
+// } ---- Q4
+
+// var array = data.items
+// for (var i = 0; i < array.length; i++) {
+// var brand = (array[i].product.brand)
+// var ebay = (array[i].product.author.name)
+// if ((brand === 'Canon') && (ebay === 'eBay')) {
+//   console.log(array[i]) }
+// } -----Q5
+
+// var array = data.items
+// for (var i = 0; i < array.length; i++) {
+//   var brand = (array[i].product.brand)
+//   var numberOfkind = (array[i].product.images)
+//   var price = (array[i].product.inventories[0].price)
+//   console.log(brand, numberOfkind, price) }
+//   ----- Q6
